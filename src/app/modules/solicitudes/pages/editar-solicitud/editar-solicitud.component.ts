e4d9ec7bfe10ef9512be8eb636df109210fc6558
AndreDaniTorres <60939382+AndreDaniTorres@users.noreply.github.com>
2021-10-01 20:40:30 -0500

    editar solicitudes

--- src/app/modules/solicitudes/pages/editar-solicitud/editar-solicitud.component.ts
+++ src/app/modules/solicitudes/pages/editar-solicitud/editar-solicitud.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone, ChangeDetectorRef } from '@angular/core';
 import { SolicitudService } from '../../../../core/services/solicitud.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Solicitud } from '../../../../core/models/solicitud';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { TipoSolicitudService } from '../../../../core/services/tipo-solicitud.service';
+import { TipoSolicitud } from '../../../../core/models/tipo-solicitud';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-editar-solicitud',
@@ -13,31 +17,73 @@ export class EditarSolicitudComponent implements OnInit {
   solicitud: Solicitud;
   getId: any;
   updateSolicitud: FormGroup;
+  loading = false;
+  submitted = false;
+  lugar = true;
+  idioma = true;
+  disponible = true;
+  // tiposSolicitud: any;
+  options$: Observable<TipoSolicitud[]>;
 
   constructor(
     private solicitudService: SolicitudService,
+    // private tipoSolicitudService: TipoSolicitudService,
     private activateRoute: ActivatedRoute,
     private formBuilder: FormBuilder,
     private ngZone: NgZone,
-    private router: Router
+    private router: Router,
+    private cd: ChangeDetectorRef
   ) {
     this.getId = this.activateRoute.snapshot.paramMap.get('id');
+
+    // this.options$ = this.tipoSolicitudService.getTipoSolicitud();
+
     this.solicitudService.getSolicitud(this.getId).subscribe((res) => {
       this.updateSolicitud.setValue({
-        tipos_solicitud_id: res.tipos_solicitud_id,
+        tipos_solicitud: res.tipos_solicitud.nombre,
         justificacion: res.justificacion,
+        lugar: res.lugar,
+        idioma: res.idioma,
       });
     });
 
     this.updateSolicitud = this.formBuilder.group({
-      tipos_solicitud_id: ['', Validators.required],
+      tipos_solicitud: [''],
       justificacion: ['', Validators.required],
+      lugar: [''],
+      idioma: [''],
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.solicitudService
+    //   .getSolicitud(this.getId)
+    //   .subscribe((res) => (this.tiposSolicitud = res));
+    // console.log('tipo solicitudddd', this.tiposSolicitud);
+    // this.tiposSolicitud = this.updateSolicitud.controls.tipos_solicitud;
+    // console.log('solcitudddd', this.tiposSolicitud);
+    // console.log(this.updateSolicitud.value);
+    // if (this.tiposSolicitud === 'comision') {
+    //   this.lugar = true;
+    //   this.idioma = true;
+    //   this.disponible = true;
+    // }
+  }
+
+  // convenience getters for easy access to form fields
+  get f() {
+    return this.updateSolicitud.controls;
+  }
 
   onUpdate(): any {
+    this.submitted = true;
+
+    // stop here if form is invalid
+
+    if (this.updateSolicitud.invalid) {
+      return;
+    }
+
     this.solicitudService
       .updateSolicitud(this.getId, this.updateSolicitud.value)
       .subscribe(() => {
